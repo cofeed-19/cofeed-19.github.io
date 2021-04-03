6747f7ff51aa6ecb136bc450d922334215b7477a
Dragoș Străinu <str.dr4605@gmail.com>
2021-04-03 20:10:13 +0300

    feat: add Usage

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,24 @@ export function Header() {
       <h1>Free web feed</h1>
       <p>No backend! All your feeds are stored in browser!</p>
       <p style={{ color: "red" }}>
-        Because we do cross-origin requests most of web feeds are blocked by CORS
-        policy😢.
+        Because we do cross-origin requests most of web feeds are blocked by
+        CORS policy😢.
       </p>
+      <details>
+        <summary>Usage</summary>
+        <p>
+          <ul>
+            <li>
+              Should work with any <u>https://[some name].github.io</u> site
+              which has web feed (rss or atom).
+            </li>
+            <li>
+              Try <u>http</u> and <u>https</u>.
+            </li>
+            <li>If it's a personal blog, maybe ask the owner to enable CORS</li>
+          </ul>
+        </p>
+      </details>
       <hr />
     </header>
   );
