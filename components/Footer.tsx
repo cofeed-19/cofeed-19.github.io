d54123e498aabf1629d929b6e3eb120958011e66
Dragoș Străinu <str.dr4605@gmail.com>
2021-04-07 00:49:43 +0300

    chore: add source code link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,21 @@ export function Footer() {
     <footer>
       <hr />© {new Date().getFullYear()}{" "}
       <a
-        href="https://github.com/strdr4605/"
+        href="https://github.com/strdr4605"
         rel="noopener noreferrer"
         target="_blank"
       >
         @strdr4605
       </a>
       . Try <code>https://strdr4605.github.io/rss.xml</code> as your first web
-      feed.
+      feed. <br />
+      <a
+        href="https://github.com/cofeed-19/cofeed-19.github.io"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        source code
+      </a>
     </footer>
   );
 }
