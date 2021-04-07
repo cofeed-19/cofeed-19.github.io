49b25c58165c96c7ca595bfc15203d337de22086
Dragoș Străinu <str.dr4605@gmail.com>
2021-04-08 00:09:43 +0300

    chore: add main url to HeadMeta

--- components/HeadMeta.tsx
+++ components/HeadMeta.tsx
@@ -10,7 +10,10 @@ export function HeadMeta() {
       />
 
       <meta property="og:title" content="Client-only Feed Reader" />
-      <meta property="og:image" content="/banner.jpg" />
+      <meta
+        property="og:image"
+        content="https://cofeed-19.github.io/banner.jpg"
+      />
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="en_US" />
       <meta
@@ -23,23 +26,23 @@ export function HeadMeta() {
       <meta name="twitter:site" content="https://cofeed-19.github.io/" />
       <meta name="twitter:creator" content="@strdr4605" />
 
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="https://cofeed-19.github.io/favicon.ico" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="https://cofeed-19.github.io/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="https://cofeed-19.github.io/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="https://cofeed-19.github.io/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
       <script async defer src="https://buttons.github.io/buttons.js"></script>
