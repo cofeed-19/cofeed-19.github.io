972ae2803d8505d9f7b000310769882fe9e6c5b6
Dragoș Străinu <str.dr4605@gmail.com>
2021-09-05 19:05:20 +0300

    feat: add ProgressLoader

---
+++ components/ProgressLoader.tsx
@@ -0,0 +1,16 @@
+export interface Props {
+  loadedFeeds: { loaded: number; total: number };
+}
+
+export function ProgressLoader({ loadedFeeds }: Props) {
+  return loadedFeeds.loaded !== loadedFeeds.total ? (
+    <h3>
+      Loading:
+      <progress
+        id="loading"
+        value={loadedFeeds.loaded}
+        max={loadedFeeds.total}
+      ></progress>
+    </h3>
+  ) : null;
+}
