b9d60ae58f0615f60232ca5ad5185de7cdaa5bec
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2021-12-17 05:10:07 +0100

    Add CORS test workflow

---
+++ scripts/test-feed-data.sh
@@ -0,0 +1,11 @@
+cat data/feeds.json | grep feedUrl | sed  's/"feedUrl"://g' | cut -d '"' -f2 > /tmp/feeds.txt
+while read URL; do
+    scripts/corstest.sh $URL
+    if [ $? -eq 0 ]; then
+        # Nothing...
+        sleep 0
+    else
+        echo "FAIL: $URL"
+        exit 1
+    fi
+done < /tmp/feeds.txt;
