da6b6f5d2ae9cfae6ccf408b6ed1dd789f88ca92
mvolosen <maximvolosenco@gmail.com>
2022-03-29 20:08:59 +0300

    [feat] created exportSevices

--- constants.ts
+++ constants.ts
@@ -2,6 +2,7 @@ export const STORAGE_PREFIX = "FWF_";
 
 export const databaseName = "FeedDb";
 export const databaseVersion = 1;
+export const siteDomain = "https://cofeed-19.github.io/";
 
 export enum SiteFeedTable {
   Name = "Site_Feed",
