e1a32e09daefca272d2a56550448690b3b6aa357
Dragoș Străinu <str.dr4605@gmail.com>
2022-12-15 16:47:35 +0200

    chore: change color for dark background
    Also format utils.ts

--- utils.ts
+++ utils.ts
@@ -1,23 +1,17 @@
 import favecon from "favecon";
 
 export function setDarkMode() {
-  document.documentElement.style.setProperty("--body-bg-color", "#1A202C");
-      document.documentElement.style.setProperty("--body-color", "#FFF");
-      document.documentElement.style.setProperty("--link-color", "#FFC400");
-      document.documentElement.style.setProperty(
-        "--link-visited-color",
-        "#C27DC0"
-      );
+  document.documentElement.style.setProperty("--body-bg-color", "#000");
+  document.documentElement.style.setProperty("--body-color", "#FFF");
+  document.documentElement.style.setProperty("--link-color", "#FFC400");
+  document.documentElement.style.setProperty("--link-visited-color", "#C27DC0");
 }
 
 export function setLightMode() {
   document.documentElement.style.setProperty("--body-bg-color", "#FFF");
-      document.documentElement.style.setProperty("--body-color", "#000");
-      document.documentElement.style.setProperty("--link-color", "#00E");
-      document.documentElement.style.setProperty(
-        "--link-visited-color",
-        "#551A8B"
-      );
+  document.documentElement.style.setProperty("--body-color", "#000");
+  document.documentElement.style.setProperty("--link-color", "#00E");
+  document.documentElement.style.setProperty("--link-visited-color", "#551A8B");
 }
 
 export async function getFavicon(link?: string): Promise<string | undefined> {
