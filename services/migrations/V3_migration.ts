d6cbfb20a4fce36032eebab30b756cc1a3629007
Dragoș Străinu <str.dr4605@gmail.com>
2025-01-17 13:19:26 +0200

    chore: add prettier

--- services/migrations/V3_migration.ts
+++ services/migrations/V3_migration.ts
@@ -1,5 +1,3 @@
-
-export async function V3_migration(db: IDBDatabase){
-
-    console.log("Third Migration")
+export async function V3_migration(db: IDBDatabase) {
+  console.log("Third Migration");
 }
