600b58eac10ae2e0f147233cd1d600806f16a11b
Dragoș Străinu <str.dr4605@gmail.com>
2026-02-21 22:03:23 +0200

    add QueryClientProvider to app root

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Header, Footer } from './components'
 import HomePage from './pages/Home'
 import FeedsPage from './pages/Feeds'
 
 type Page = 'home' | 'feeds'
 
+const queryClient = new QueryClient()
+
 export default function App() {
   const [page, setPage] = useState<Page>('home')
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header onNavigate={(p) => setPage(p as Page)} />
       <main>
         {page === 'home' && <HomePage />}
         {page === 'feeds' && <FeedsPage />}
       </main>
       <Footer />
-    </>
+    </QueryClientProvider>
   )
 }
